e2dfcbf90447e9c46c1a0a41462dd8297357d41a
Vitor Gabriel <sgvitor@outlook.com.br>
2024-11-04 15:13:42 -0300

    feat: models matricula, curso e categoria

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ import app from "./src/app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor iniciado em http://localhost:" + PORT);
+    console.log(`Servidor iniciado em http://localhost:${PORT}`);
 });
