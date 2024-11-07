f154fad2bf3a685f3ac3d8626dd7ae14ce98bf5d
Vitor Gabriel <sgvitor@outlook.com.br>
2024-11-07 00:57:07 -0300

    feat: find all pessoas

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,12 +1,16 @@
+const model = require("../models");
+
 class PessoaController {
     static async pegaPessoasAtivas(req, res) {
         try {
-            const pessoasAtivas = await Pessoa.findAll({
-                where: { isAtivo: true },
+            const pessoasAtivas = await model.Pessoa.findAll({
+                where: { ativo: true },
             });
-            res.status(200).json(pessoasAtivas);
+            return res.status(200).json(pessoasAtivas);
         } catch (error) {
-            res.status(500).json({ message: "Erro ao buscar pessoas ativas" });
+            return res
+                .status(500)
+                .json({ message: "Erro ao buscar pessoas ativas" });
         }
     }
 }
